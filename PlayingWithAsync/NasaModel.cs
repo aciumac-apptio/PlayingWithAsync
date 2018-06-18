0ef57489568232f0f5d9cd7e6f560b88760fbac8
Artem <aciumac@vertafore.com>
2018-06-18 16:51:37 -0700

    Playing with async/sync methods

---
+++ PlayingWithAsync/NasaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingWithAsync
+{
+    class NasaModel
+    {
+        public string Date { get; set; }
+        public string Explaination { get; set; }
+        public string Media_Type { get; set; }
+        public string Service_Version { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+
+    }
+}
---
+++ PlayingWithAsync/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlayingWithAsync
+{
+    public class Program
+    {
+        public static readonly string BaseUrl_NASA = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Тоже самое что Task.Result только чуть безопаснее
+            //var res = Task.Run(() => GetUrl()).GetAwaiter().GetResult();
+            // https://cpratt.co/async-tips-tricks/
+            var res = GetUrl().GetAwaiter().GetResult();
+            Console.WriteLine(res);
+        }
+
+        public static async Task<string> GetUrl()
+        {
+            using(var client = new HttpClient())
+            {
+                using(var response = await client.GetAsync(BaseUrl_NASA).ConfigureAwait(false))
+                {
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+    }
+}
